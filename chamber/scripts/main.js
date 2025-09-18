269ba7a5623d251a9bfacb42ae2ab747771a6e2a
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-09-17 22:04:34 -0300

    final

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -18,3 +18,30 @@
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
     });
+
+// Members
+document.addEventListener("DOMContentLoaded", () => {
+const membersContainer = document.getElementById("members");
+
+    // JSON
+    fetch("data/members.json")
+        .then(response => response.json())
+        .then(members => {
+            members.forEach(member => {
+
+                // Cards (image, name, adress, phone, website)
+                const card = document.createElement("div");
+                card.classList.add("member-card");
+
+                card.innerHTML = 
+                    `<img src="images/${member.image}" alt="${member.name}">
+                    <h2>${member.name}</h2>
+                    <p>${member.address}</p>
+                    <p>${member.phone}</p>
+                    <p>${member.membership}</p>
+                    <a href="${member.website}" target="_blank">Visit Website</a>`;
+                membersContainer.appendChild(card);
+            });
+
+        })
+});
