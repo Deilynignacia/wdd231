4e1053158d2db118a6a7f7da145313487938ef77
Deilyn Zamudio <deilynzamudio@gmail.com>
2025-10-04 22:07:18 -0300

    .

---
+++ chamber/scripts/join.js
@@ -0,0 +1,20 @@
+// Footer
+    // Current Year
+    const currentYearSpan = document.getElementById('currentyear');
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    currentYearSpan.textContent = currentYear;
+
+    // Last Modified
+    const lastModifiedParagraph = document.getElementById('lastModified');
+    const lastModifiedDate = document.lastModified; 
+    lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
+
+// Menu
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
